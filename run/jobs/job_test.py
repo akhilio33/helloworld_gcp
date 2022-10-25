f360c260f9157e1e67a8d82c3b6cc16b9fdacc04
Akhil <ahchav33@gmail.com>
2022-10-25 09:39:20 -0700

    Try running script within run/jobs.job_test.py

--- run/jobs/job_test.py
+++ run/jobs/job_test.py
@@ -4,9 +4,9 @@
 # import sys
 # import time
 
+import wrappers.wrapper_test
 import sys
 sys.path.append('../..')
-import wrappers.wrapper_test
 
 
 # Define main script
@@ -15,13 +15,14 @@ def hello_run():
   print(f"Hello World!! - from run/jobs/hello_run")
 
   wt = wrappers.wrapper_test.testObject()
-  wt.test_func('tst str passed to wrapper test_func from run/jobs/job_test.hello_run')
+  wt.test_func(
+      'tst str passed to wrapper test_func from run/jobs/job_test.hello_run')
 
 
-# # Start script
-# if __name__ == "__main__":
-#   try:
-#     hello_run()
-#   except Exception as err:
-#     message = f"{str(err)}"
-#     print(message)
+# Start script
+if __name__ == "__main__":
+  try:
+    hello_run()
+  except Exception as err:
+    message = f"{str(err)}"
+    print(message)
