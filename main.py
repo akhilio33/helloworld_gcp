7007cd3e29bb34d12cfe65ef442c98c99dbf51ef
Akhil <ahchav33@gmail.com>
2022-10-31 12:22:45 -0700

    testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import functions.function_test
+import functions.function_test_2
 # import run.jobs.job_test1
 import pandas as pd
 
@@ -9,11 +10,12 @@ def call_func(event, context):
   df = pd.DataFrame(data, columns=['test'])
   print(df)
 
-# def call_func(event, context):
-#   functions.function_test.hello_func()
-#   data = ['hello', 'from', 'main.call_func']
-#   df = pd.DataFrame(data, columns=['test'])
-#   print(df)
+
+def call_func_2(event, context):
+  functions.function_test_2.hello_func_2()
+  data = ['hello', 'from', 'main.call_func_2']
+  df = pd.DataFrame(data, columns=['test'])
+  print(df)
 
 
 # def call_job():
